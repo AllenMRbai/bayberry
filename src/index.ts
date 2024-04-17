700b81303c5425062515d1787c5308bd6e828c43
bairongyu <bairongyu@kuaishou.com>
2024-04-17 17:43:07 +0800

    feat: 完成一个简单的store

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { useSyncExternalStore } from "react";
+
+type Listener = () => void;
+
+export const create = <StateT extends Record<string, any>>(
+  initialState: StateT
+) => {
+  let state = initialState;
+  const listeners = [] as Listener[];
+
+  const get = () => state;
+
+  const set = (st: any) => {
+    const newState = typeof st == "function" ? st(state) : st;
+    state = { ...state, ...newState };
+
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener: Listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners.filter((l) => l !== listener);
+    };
+  };
+
+  const getInitialState = () => initialState;
+
+  const use = () => {
+    return useSyncExternalStore(subscribe, get, getInitialState);
+  };
+
+  const store = {
+    get,
+    set,
+    subscribe,
+    use,
+  };
+
+  return store;
+};
